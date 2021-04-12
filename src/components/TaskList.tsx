479ffe72773d9bc28721551cc551a5e670b414b7
Richard Ben <frichardben@yahoo.com.br>
2021-04-12 07:44:28 -0300

    feat: challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,38 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle.trim() === '' ) {
+      //throw new Error('Por favor, preencha o campo')
+      alert('Por favor, preenche o campo')
+      return
+    } 
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldTask => [...oldTask, newTask]);
+    setNewTaskTitle('')
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const completedTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(completedTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTask = tasks.filter(task => task.id !== id)
+
+    setTasks(filteredTask)
   }
 
   return (
